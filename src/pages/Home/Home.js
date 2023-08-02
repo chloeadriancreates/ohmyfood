edf59d0f260352662e90f16c1dabaf2785a38dde
chloeadriancreates <chloeadriancreates@gmail.com>
2023-08-02 12:13:41 +0200

    Add Hero section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import "./Home.scss";
 import Header from "../../components/Header/Header";
 import Location from "./sections/Location/Location";
+import Hero from "./sections/Hero/Hero";
 
 export default function Home() {
     return (
         <div>
             <Header back={false} />
             <Location />
+            <Hero />
         </div>
     )
 }
---
+++ src/pages/Home/sections/Hero/HeroCard/HeroCard.js
@@ -0,0 +1,13 @@
+import "./HeroCard.scss";
+
+export default function HeroCard({number, icon, caption}) {
+    return (
+        <div className="heroCard">
+            <p className="heroCard_number">{number}</p>
+            <div className="heroCard_caption">
+                <i className={`heroCard_caption_icon fas ${icon}`}></i>
+                <p className="heroCard_caption_text">{caption}</p>
+            </div>
+        </div>
+    )
+}
